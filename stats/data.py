a20a16a9d372f288e5572cf2c110c58eeeb520a7
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2020-06-07 18:19:39 +0100

    Update data.py

--- stats/data.py
+++ stats/data.py
@@ -1,6 +1,5 @@
 import os
 import glob
-import matplotlib
 import pandas as pd
 
 game_files = glob.glob(os.path.join(os.getcwd(),"games",'*.EVE'))
@@ -9,8 +8,8 @@ game_files.sort()
 
 game_frames = []
 for game_file in game_files:
-    game_frame = pd.read_csv(game_file, names = ["type",'multi2','multi3','multi4','multi5','multi6','event']
-    game_frames.append(game_pframe)
+    game_frame = pd.read_csv(game_file, names = ["type",'multi2','multi3','multi4','multi5','multi6','event'])
+    game_frames.append(game_frame)
 
 games = pd.concat(game_frames)
 
@@ -24,6 +23,6 @@ identifiers.columns = ['game_id','year'] # .columns permits setting of column la
 
 games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna('')
-games.loc[[:,'type']] = pd.Categorical(games.loc[[:,'type']]) # : denotes all rows
+games.loc[:,'type'] = pd.Categorical(games.loc[:,'type']) # : denotes all rows
 
 print(games.head())
